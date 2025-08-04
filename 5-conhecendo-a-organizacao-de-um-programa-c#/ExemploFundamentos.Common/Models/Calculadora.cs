d759762337a439ae45adebc014da549d12eacc99
Felipe-Noguez <felipenoguez@outlook.com.br>
2025-08-03 22:16:27 -0300

    feat: conhecendo a organizacao de um projeto c#

---
+++ 5-conhecendo-a-organizacao-de-um-programa-c#/ExemploFundamentos.Common/Models/Calculadora.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace operadores_aritmeticos_em_c_.Common.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"A soma de {x} + {y} é igual a {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"A subtração de {x} - {y} é igual a {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"A multiplicação de {x} * {y} é igual a {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"A divisão de {x} / {y} é igual a {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            Console.WriteLine($"A potencia de {x} ^ {y} é igual a {Math.Pow(x, y)}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radianos = angulo * (Math.PI / 180);
+            double seno = Math.Sin(radianos);
+            Console.WriteLine($"O seno de {angulo} graus é igual a {Math.Round(seno, 4)}");
+            // Alternativa sem conversão de graus para radianos
+            Console.WriteLine($"O seno de {angulo}º graus é igual a {Math.Sin(radianos)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radianos = angulo * (Math.PI / 180);
+            double coseno = Math.Cos(radianos);
+            Console.WriteLine($"O coseno de {angulo}º é igual a {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radianos = angulo * (Math.PI / 180);
+            double tangente = Math.Tan(radianos);
+            Console.WriteLine($"A tangente de {angulo}º é igual a {Math.Round(tangente, 4)}");
+        }
+        
+        public void RaizQuadrada(double numero)
+        {
+            if (numero < 0)
+            {
+                Console.WriteLine("Não é possível calcular a raiz quadrada de um número negativo.");
+            }
+            else
+            {
+                double raiz = Math.Sqrt(numero);
+                Console.WriteLine($"A raiz quadrada de {numero} é igual a {Math.Round(raiz, 4)}");
+            }
+        }
+    }
+}
---
+++ 5-conhecendo-a-organizacao-de-um-programa-c#/Pessoa/Program.cs
@@ -0,0 +1,79 @@
+﻿// ################################# organizando e referenciando projetos #################################
+using sintaxe_e_tipos_de_dados.Common.Models;
+using operadores_aritmeticos_em_c_.Common.Models;
+
+Pessoa pessoa = new Pessoa("Felipe", 42);
+pessoa.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string opcao;
+// bool exibirMenu = true;
+
+// while(exibirMenu)
+// {
+//     // Console.Clear(); // Limpa a tela para uma melhor visualização
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1. Cadastrar cliente");
+//     Console.WriteLine("2. Buscar cliente");
+//     Console.WriteLine("3. Excluir cliente");
+//     Console.WriteLine("4. Sair");
+//     Console.WriteLine("Escolha uma opção: ");
+
+//     opcao = Console.ReadLine();
+
+//     switch (opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastrar cliente.");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Buscar cliente.");
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Apagar cliente.");
+//             break;
+
+//         case "4":
+//             Console.WriteLine("Saindo do programa...");
+//             // Environment.Exit(0); // Encerra o programa imediatamente
+//             exibirMenu = false; // Define que não deve exibir o menu novamente
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção inválida. Tente novamente.");
+//             break;
+//     }
+// }
